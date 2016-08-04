31ea97fe0fd6305cecbd64b1b3e4ec513880884b
Ryland Bell <ryland.bell@gmail.com>
2016-08-04 12:30:36 -0700

    elements representing part of current path are now marked as active; elements representing non-objects are marked disabled

--- js/main.jsx
+++ js/main.jsx
@@ -7,12 +7,12 @@ var JSONExplorer = React.createClass({
       <div className="container">
         <div className="row">
           <div className="col-xs-12 col-md-8">
-            <ColumnView data={this.props.data} currentPathArray={this.props.currentPathArray}/>
+            <ColumnView data={this.props.data} currentPath={this.props.currentPath}/>
           </div>
         </div>
         <div className="row">
           <div className="col-xs-12 col-md-8">
-            <PathView currentPathArray={this.props.currentPathArray}/>
+            <PathView currentPath={this.props.currentPath}/>
           </div>
         </div>
       </div>
@@ -22,14 +22,15 @@ var JSONExplorer = React.createClass({
 
 var ColumnView = React.createClass({
   render: function() {
+    var currentPath = this.props.currentPath;
 
     //get array of all visible levels (as values), beginning with the full data object and getting more specific
-    var visibleLevels = getAllLevels(this.props.data,this.props.currentPathArray);
+    var visibleLevels = getAllLevels(this.props.data,currentPath);
 
     //convert the levels from JS values to LevelColumn components
-    visibleLevels = visibleLevels.map(function(level){
+    visibleLevels = visibleLevels.map(function(levelContent,levelDepth){
       return (
-        <LevelColumn data={level} />
+        <LevelColumn data={levelContent} levelDepth={levelDepth} currentPath={currentPath}/>
       );
     });
 
@@ -45,14 +46,25 @@ var ColumnView = React.createClass({
 var LevelColumn = React.createClass({
   render: function() {
     var keyRows = [];
+    var levelDepth = this.props.levelDepth;
+    var currentPath = this.props.currentPath;
 
-    //if the column represents an object, list its values as rows; otherwise, list its value
+    //if the column represents an object, print its keys as rows
     if (typeof this.props.data === 'object'){
+      var markActive;
+
+      //test if each entry is part of the currently selected path
       for (var key in this.props.data){
-        keyRows.push(<KeyRow keyName={key} />);
+        markActive = false;
+        if (currentPath[levelDepth] == key){
+          markActive = true;
+        }
+        keyRows.push(<KeyRow keyName={key} isActive={markActive} isDisabled={false}/>);
       }
+
+    //for non-object columns, just print the value as a single, click-disabled row
     } else {
-      keyRows.push(<KeyRow keyName={this.props.data} />);
+      keyRows.push(<KeyRow keyName={this.props.data} isActive={false} isDisabled={true} />);
     }
 
     return (
@@ -69,7 +81,11 @@ var LevelColumn = React.createClass({
 var KeyRow = React.createClass({
   render: function() {
     return (
-      <div className="list-group-item key-row">{this.props.keyName}</div>
+      <a className={
+        "list-group-item key-row "
+        +(this.props.isDisabled ? 'disabled' : '')
+        +(this.props.isActive ? 'active' : '')}
+      >{this.props.keyName}</a>
     );
   }
 });
@@ -93,14 +109,14 @@ var PathView = React.createClass({
     return (
       <div className="path-view">
         <div className="path-caption">Current path:</div>
-        <div className="current-path lead">{this.props.currentPathArray.join('.')}</div>
+        <div className="current-path lead">{this.props.currentPath.join('.')}</div>
       </div>
     );
   }
 });
 
 ReactDOM.render(
-  <JSONExplorer data={fudge} currentPathArray={initialPath}/>, document.getElementById('app-container')
+  <JSONExplorer data={fudge} currentPath={initialPath}/>, document.getElementById('app-container')
 );
 
 // Helper functions:
