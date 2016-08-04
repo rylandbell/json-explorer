5a82543f278fc670a7bfe1f9295748f56736895c
Ryland Bell <ryland.bell@gmail.com>
2016-08-03 21:44:21 -0700

    now recursively displays levels, given a path as an array

--- js/main.jsx
+++ js/main.jsx
@@ -1,4 +1,5 @@
 var fudge = JSON.parse('{"name":"Ryland","age":34,"cities":["Portland","Eugene"],"myObj":{"magic":35}}');
+var initialPath = ['cities',0];
 
 var JSONExplorer = React.createClass({
   render: function() {
@@ -6,12 +7,12 @@ var JSONExplorer = React.createClass({
       <div className="container">
         <div className="row">
           <div className="col-xs-12 col-md-8">
-            <ColumnView data={this.props.data}/>
+            <ColumnView data={this.props.data} currentPathArray={this.props.currentPathArray}/>
           </div>
         </div>
         <div className="row">
           <div className="col-xs-12 col-md-8">
-            <PathView curentPathArray={this.props.currentPathArray}/>
+            <PathView currentPathArray={this.props.currentPathArray}/>
           </div>
         </div>
       </div>
@@ -21,9 +22,21 @@ var JSONExplorer = React.createClass({
 
 var ColumnView = React.createClass({
   render: function() {
+
+    //get array of all visible levels (as values), beginning with the full data object and getting more specific
+    var visibleLevels = getAllLevels(this.props.data,this.props.currentPathArray);
+
+    //convert the levels from JS values to LevelColumn components
+    visibleLevels = visibleLevels.map(function(level){
+      return (
+        <LevelColumn data={level} />
+      );
+    });
+
+    //draw all of the LevelColumn components:
     return (
       <div className="column-view">
-        <LevelColumn data={this.props.data} />
+        {visibleLevels}
       </div>
     );
   }
@@ -32,9 +45,16 @@ var ColumnView = React.createClass({
 var LevelColumn = React.createClass({
   render: function() {
     var keyRows = [];
-    for (var key in this.props.data){
-      keyRows.push(<KeyRow keyName={key} />);
+
+    //if the column represents an object, list its values as rows; otherwise, list its value
+    if (typeof this.props.data === 'object'){
+      for (var key in this.props.data){
+        keyRows.push(<KeyRow keyName={key} />);
+      }
+    } else {
+      keyRows.push(<KeyRow keyName={this.props.data} />);
     }
+
     return (
       <div className="level-column">
         <div className="list-group">
@@ -73,12 +93,30 @@ var PathView = React.createClass({
     return (
       <div className="path-view">
         <div className="path-caption">Current path:</div>
-        <div className="current-path lead">{this.props.curentPathArray.join('.')}</div>
+        <div className="current-path lead">{this.props.currentPathArray.join('.')}</div>
       </div>
     );
   }
 });
 
 ReactDOM.render(
-  <JSONExplorer data={fudge} currentPathArray={['scooby','doo']}/>,document.getElementById('app-container')
+  <JSONExplorer data={fudge} currentPathArray={initialPath}/>, document.getElementById('app-container')
 );
+
+// Helper functions:
+function getAllLevels (data, path){
+  var allLevels = [data];
+
+  function addLevelAndChildren (subData, subPath){
+    var nextKey = subPath[0];
+    allLevels.push(subData[nextKey]);
+    if (subPath.length>1){
+      var newSubPath = subPath.slice(1);
+      addLevelAndChildren(subData[nextKey],newSubPath);
+    }
+  }
+  addLevelAndChildren (data,path);
+
+  return allLevels;
+}
+
