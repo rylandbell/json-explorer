b4a61a7590dd20d1e2d1b1a21bb88b381a0a664b
Ryland Bell <ryland.bell@gmail.com>
2016-08-09 11:43:13 -0700

    translates ExplorerApp to fat arrow function

--- js/main.jsx
+++ js/main.jsx
@@ -1,35 +1,33 @@
 // ------------------React components:-------------------
-var ExplorerApp = React.createClass({
-  render: function() {
-    return (
-      <div className="container ">
-        <div className="row">
-          <div className="col-xs-12 col-md-12">
-            <p className="non-app-text">Don't have any JSON strings handy? <a href="" data-toggle="modal" data-target="#sample-strings">Click here</a> for a few samples to copy and paste.</p>
-          </div>
-        </div>
-        <div className="row main-app-row">
-          <div className="col-xs-12 col-md-4">
-            <InputPane textContent={this.props.reduxState.textContent} handleTextChange={this.props.handleTextChange} handleFormSubmit={this.props.handleFormSubmit}/>
-          </div> 
-          <div className="col-xs-12 col-md-8">
-            <ExplorerPane data= {this.props.reduxState.data} currentPath= {this.props.reduxState.currentPath} updatePath= {this.props.updatePath}/>
-          </div>  
-        </div>
-        <div className="row">
-          <div>
-            <div className={"error-msg alert alert-danger "+(this.props.reduxState.showError ? "":"hidden")} role="alert">Sorry, but that doesn't appear to be a valid JSON string. Please try again.</div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-xs-12">
-            <ContentPane data= {this.props.reduxState.data} currentPath= {this.props.reduxState.currentPath} />
-          </div>
+var ExplorerApp = ({reduxState,handleTextChange,handleFormSubmit,updatePath}) => {
+  return (
+    <div className="container ">
+      <div className="row">
+        <div className="col-xs-12 col-md-12">
+          <p className="non-app-text">Don't have any JSON strings handy? <a href="" data-toggle="modal" data-target="#sample-strings">Click here</a> for a few samples to copy and paste.</p>
         </div>
       </div>
-    );
-  }
-});
+      <div className="row main-app-row">
+        <div className="col-xs-12 col-md-4">
+          <InputPane textContent={reduxState.textContent} handleTextChange={handleTextChange} handleFormSubmit={handleFormSubmit}/>
+        </div> 
+        <div className="col-xs-12 col-md-8">
+          <ExplorerPane data= {reduxState.data} currentPath= {reduxState.currentPath} updatePath= {updatePath}/>
+        </div>  
+      </div>
+      <div className="row">
+        <div>
+          <div className={"error-msg alert alert-danger "+(reduxState.showError ? "":"hidden")} role="alert">Sorry, but that doesn't appear to be a valid JSON string. Please try again.</div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-12">
+          <ContentPane data= {reduxState.data} currentPath= {reduxState.currentPath} />
+        </div>
+      </div>
+    </div>
+  );
+}
 
 //Contains the text input and left 1/3 of the app
 var InputPane = ({textContent,handleTextChange,handleFormSubmit}) => {
